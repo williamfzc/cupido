765c2a5f336c09cde2fa70b3c1107dc46a75714e
williamfzc <178894043@qq.com>
2024-01-19 22:25:30 +0800

    chore: log system

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use tracing::info;
 use cupid::collector::{Config, walk};
 use cupid::server::{server_main, ServerConfig};
 
 fn main() {
+    tracing_subscriber::fmt::init();
+
+    info!("graph creating ...");
     let conf = Config::new(".", 10240);
     let graph = walk(conf);
 
     let server_conf = ServerConfig::new(graph);
+    info!("server up");
     server_main(server_conf);
 }
