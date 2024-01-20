3e3c843b57ea181a9ee6dd478b95c597632cf53c
williamfzc <178894043@qq.com>
2024-01-20 20:34:56 +0800

    feat: cmd

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use clap::Command;
 use cupid::collector::{walk, Config};
 use cupid::server::{server_main, ServerConfig};
 use tracing::info;
 
 fn main() {
+    let cmd = Command::new("cupid")
+        .bin_name("cupid")
+        .subcommand_required(true)
+        .subcommand(Command::new("up").about("cupid server up"));
+    let matches = cmd.get_matches();
+    match matches.subcommand() {
+        Some(("up", _matches)) => handle_up(),
+        _ => unreachable!("clap should ensure we don't get here"),
+    };
+}
+
+fn handle_up() {
     tracing_subscriber::fmt::init();
 
     info!("graph creating ...");
